028f28bf42a31a229975341a719a4e178577ecc4
Jesse <209702098+LiesJC@users.noreply.github.com>
2026-01-19 16:53:47 -0400

    Modulo1

---
+++ archivo.py
@@ -0,0 +1,42 @@
+#Hola mundo
+print("Hola mundo")
+
+#COMENTARIOS "#"
+#PROGRAMACION ESTRUCTURADA
+a = 10
+b = 20
+suma = a + b
+resta = a - b
+multiplacion = a * b
+division = a / b
+modulo = a % b
+
+print("El valor de la suma es: " , suma)
+print("El valor de la resta es: " , resta)
+print("El valor de la multiplicacion es: " , multiplacion)
+print("El valor de la division es: " , division)
+print("El valor de la modulo es: " , modulo)
+
+#Tipos de datos
+CONSTANTE = 3
+
+num = 10000
+caracter = 'hola mundo'
+imaginario = 1+3j
+decimal = 3.4545
+
+print(type(num))
+print(type(caracter))
+print(type(imaginario))
+print(type(decimal))
+
+#Prueba de caracteres 
+num1 = 3
+num2 = '4'
+suma1 = num1 + int(num2)
+
+print(suma1)
+
+#Entrada de datos
+entrada  = int(input("Escriba un dato: "))
+print(type(entrada))
---
+++ conexion.py
@@ -0,0 +1,9 @@
+datos = input("Ingresamos datos: ")
+
+print("ADMINISTRACION PARA BASE DE DATOS")
+print("1. Leer datos")
+print("2. Escribir datos")
+print("3. Salir")
+
+print("Los datos son: ", datos)
+
